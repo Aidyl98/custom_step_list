228b96d97a4f9aa3c8678caddeb176a1a31895a6
Aidyl <jgarciaalbalah@gmail.com>
2022-08-04 21:34:45 -0400

    CustomStep: + title, subtitle, endOfLineContent, isActive and state

---
+++ lib/custom_step.dart
@@ -0,0 +1,37 @@
+import 'package:custom_step_list/custom_step_state.dart';
+import 'package:flutter/material.dart';
+
+class CustomStep {
+  /// The title of the step that typically describes it.
+  final Widget title;
+
+  /// The subtitle of the step that appears below the title and has a smaller
+  /// font size. It typically gives more details that complement the title.
+  ///
+  /// If null, the subtitle is not shown.
+  final Widget? subtitle;
+
+  /// The content of the step that appears below the line that conect
+  /// a line with another.
+  ///
+  /// If null, the endOfLineContent is not shown.
+  final Widget? endOfLineContent;
+
+  /// The state of the step which determines the styling of its components
+  /// and whether steps are interactive.
+  final CustomStepState state;
+
+  /// Whether or not the step is active. The flag only influences styling.
+  final bool isActive;
+
+  /// Creates a step for a [CustomStepListWidget].
+  ///
+  /// The [title] argument must not be null.
+  const CustomStep({
+    required this.title,
+    this.subtitle,
+    this.endOfLineContent,
+    this.state = CustomStepState.normal,
+    this.isActive = true,
+  });
+}
