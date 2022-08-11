b554c4147f527fe5d903dbb64ca254e8fa79703c
Aidyl <jgarciaalbalah@gmail.com>
2022-08-11 09:58:17 -0400

    CustomStepListExample: working example

--- example/custom_step_list_example.dart
+++ example/custom_step_list_example.dart
@@ -1,14 +1,23 @@
+import 'package:custom_step_list/custom_step.dart';
+import 'package:custom_step_list/custom_step_list_widget.dart';
+import 'package:custom_step_list/custom_step_state.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: CustomStepListExample(),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const CustomStepListExample(),
     );
   }
 }
@@ -16,11 +25,69 @@ class MyApp extends StatelessWidget {
 class CustomStepListExample extends StatelessWidget {
   const CustomStepListExample({Key? key}) : super(key: key);
 
+  CustomStep _buildStep({
+    required Widget title,
+    CustomStepState state = CustomStepState.normal,
+    Widget? circleChild,
+  }) {
+    return CustomStep(
+      title: title,
+      state: state,
+      subtitle: const Text("Subtitle"),
+      circleChild: circleChild,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      backgroundColor: Colors.grey.shade100,
       body: SafeArea(
-        child: Text("Example"),
+        child: CustomStepListWidget(
+          stepListPadding: const EdgeInsets.all(16.0),
+          circleColor: Colors.blue,
+          showCircleChildByState: true,
+          steps: [
+            _buildStep(
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text('Some Title'),
+                  Text(
+                    "Some Info",
+                  ),
+                ],
+              ),
+              circleChild: const Center(
+                child: Text("1"),
+              ),
+            ),
+            _buildStep(
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text('Some Title'),
+                  Text(
+                    "Some Info",
+                  ),
+                ],
+              ),
+              state: CustomStepState.complete,
+            ),
+            _buildStep(
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text('Some Title'),
+                  Text(
+                    "Some Info",
+                  ),
+                ],
+              ),
+              state: CustomStepState.error,
+            ),
+          ],
+        ),
       ),
     );
   }
